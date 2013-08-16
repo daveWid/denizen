c044f1765b4ab542ed815155d6c30fe1b813e711
Dave Widmer <dwidmer@bgsu.edu>
2013-08-16 16:03:25 -0400

    Adding some helpers into the application.

--- src/Denizen/Application.php
+++ src/Denizen/Application.php
@@ -2,9 +2,17 @@
 
 namespace Denizen;
 
+use \League\OAuth2\Server\Authorization,
+	\League\OAuth2\Server\Resource,
+	\League\OAuth2\Server\Util\Request,
+	\League\OAuth2\Server\Grant\ClientCredentials,
+	\League\OAuth2\Server\Grant\Password;
+
 class Application extends \Slim\Slim
 {
 	private $pdo = null;
+	private $serverAuth = null;
+	private $serverResource = null;
 
 	/**
 	 * Gets the url for the site. Useful for applications that are in subdirectories
@@ -35,6 +43,61 @@ class Application extends \Slim\Slim
 		}
 
 		return $this->pdo;
+	}
+
+	public function getAuthServer()
+	{
+		if ($this->serverAuth === null)
+		{
+			$this->serverAuth = new Authorization(
+				new \Denizen\OAuth2\Client($this->getPDO()),
+				new \Denizen\OAuth2\Session($this->getPDO()),
+				new \Denizen\OAuth2\Scope($this->getPDO())
+			);
+
+			$this->serverAuth->setRequest(Request::buildFromGlobals());
+
+			$this->serverAuth->addGrantType(new Password($this->serverAuth));
+			$this->serverAuth->addGrantType(new ClientCredentials($this->serverAuth));
+		}
+
+		return $this->serverAuth;
+	}
+
+	public function getResourceServer()
+	{
+		if ($this->serverResource === null)
+		{
+			$this->serverResource = new Resource(
+				new \Denizen\OAuth2\Session($this->getPDO())
+			);
+
+			$this->serverResource->setRequest(Request::buildFromGlobals());
+		}
+
+		return $this->serverResource;
+	}
+
+	/**
+	 * Creates a new controller while injecting the app into it.
+	 *
+	 * @param  string $name The name of the controller
+	 * @return mixed        The controller with the app injected
+	 */
+	public function controller($name)
+	{
+		return new $name($this);
+	}
+
+	/**
+	 * Creates an instance of a \Peyote\PDO table class with the PDO injected.
+	 *
+	 * @param  string $name  Name of the class to create
+	 * @return mixed         The class with the pdo instance injected
+	 */
+	public function table($name)
+	{
+		return new $name($this->getPDO());
 	}
 
 }
