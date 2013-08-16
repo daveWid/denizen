d568496fe7f7142509e85f1c41a0bb46a968aed6
Dave Widmer <dwidmer@bgsu.edu>
2013-08-16 12:12:14 -0400

    Adding some helper methods into the Application.

--- src/Denizen/Application.php
+++ src/Denizen/Application.php
@@ -4,5 +4,37 @@ namespace Denizen;
 
 class Application extends \Slim\Slim
 {
+	private $pdo = null;
+
+	/**
+	 * Gets the url for the site. Useful for applications that are in subdirectories
+	 *
+	 * Set <code>site.url</code> when creating the application instance.
+	 *
+	 * @return string
+	 */
+	public function getSiteUrl()
+	{
+		$url = $this->config('site.url', '');
+		return \rtrim($url, '/');
+	}
+
+	/**
+	 * Gets a global PDO instance used for database interactions.
+	 *
+	 * Set <code>db</code> when creating the application instance.
+	 *
+	 * @return [type] [description]
+	 */
+	public function getPDO()
+	{
+		if ($this->pdo === null)
+		{
+			$config = $this->config('db');
+			$this->pdo = new \PDO($config['dsn'], $config['user'], $config['password']);
+		}
+
+		return $this->pdo;
+	}
 
 }
